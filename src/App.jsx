bf3243e073fe82b275a4aae71eb3d4e21ccc9cf5
Zia Sahil <sahilmohammadzia@gamil.com>
2025-12-27 12:38:14 +0430

    create a good react project for a podcast web page

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Content from "./components/Content"
+import Header from "./components/Header"
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <Header />
+    <Content />
+    </>
+  )
+}
+
+export default App
